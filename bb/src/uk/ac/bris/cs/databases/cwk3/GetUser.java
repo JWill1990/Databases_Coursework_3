3862bc9160652acb93645a25f056c4673de530a4
xtrememib01 <xtrememib01@gmail.com>
2016-04-19 18:16:22 +0100

    Added a separate method for get user
    TO make the api.java look clean

---
+++ bb/src/uk/ac/bris/cs/databases/cwk3/GetUser.java
@@ -0,0 +1,42 @@
+package uk.ac.bris.cs.databases.cwk3;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import uk.ac.bris.cs.databases.api.Result;
+
+public class GetUser {
+	  
+    public static  Result<Map<String, String>> GetUserInfo(Connection c){ 
+    	PreparedStatement pstmt;
+        ResultSet rst;
+    	String sql="SELECT name,username FROM Person";
+        Map<String, String> maps=new HashMap<String,String>();  //Maps
+    	try {
+		pstmt = c.prepareStatement(sql);
+		rst=pstmt.executeQuery();
+		System.out.println("test");
+        while(rst.next()){
+          		 String name=rst.getString("name").trim();
+  		         String username=rst.getString("username").trim();
+  		         System.out.println(name+","+username);
+  		         System.out.println(rst.getRow());
+  		         maps.put(name,username);
+  	 	  }
+      
+        /*  condition to check if the map/databse is empty*/
+      if(!maps.isEmpty()){
+    	return Result.success(maps);
+      }
+      else{
+    	return Result.failure("There are no user for this table");
+      }
+    } catch (SQLException e) {}
+    return Result.fatal("Unknown error");
+}
+}
+

2708afa7a72e055d89781b4b129eb9b28f3cb01e
Jack Williams <jw15326@it025956.users.bris.ac.uk>
2016-04-28 17:04:42 +0100

    added getForums

--- bb/src/uk/ac/bris/cs/databases/cwk3/Forum.java
+++ bb/src/uk/ac/bris/cs/databases/cwk3/Forum.java
@@ -11,18 +11,21 @@ import java.util.Map;
 
 import uk.ac.bris.cs.databases.api.Result;
 import uk.ac.bris.cs.databases.api.SimpleForumSummaryView;
+import uk.ac.bris.cs.databases.api.ForumSummaryView;
 
 public class Forum {
 
-    private final static String simpleForumSummaryViewStatement = "SELECT * FROM Forum";
+    private final static String simpleForumSummaryViewStatement = "SELECT id, title FROM Forum";
+    private final static String forumSummaryViewStatement = "SELECT * FROM Forum ORDER BY Forum.title";
+    private final static String topicSummaryStatement = "SELECT id, title FROM Topic;
 
-    public static  Result<List<SimpleForumSummaryView>> getSummary(Connection c){ 
+    public static  Result<List<SimpleForumSummaryView>> getSimpleSummary(Connection c){ 
         ResultSet rst;
         ArrayList list = new ArrayList();
         try (PreparedStatement pstmt= c.prepareStatement(simpleForumSummaryViewStatement)){
             rst=pstmt.executeQuery();
             while(rst.next()){
-                Long id=rst.getLong("id");
+                long id = rst.getLong("id");
                 String title=rst.getString("title").trim();
                 list.add(new SimpleForumSummaryView(id,title));
             }
@@ -32,12 +35,57 @@ public class Forum {
                 return Result.success(list);
             }
             else{
-                return Result.failure("There are no user for this table");
+                return Result.failure("There are no forums for this table");
             }
         }
         catch (SQLException e) {
             return Result.fatal("Unknown error");
         }
+    }
+
+    /**
+     * Get the "main page" containing a list of forums ordered alphabetically
+     * by title.
+     * @return the list of all forums, empty list if there are none.
+     */
+
+    public static Result<List<ForumSummaryView>> getSummary(Connection c)
+    {
+        ResultSet rst;
+        ResultSet secondRst;
+        ArrayList forumList = new ArrayList();
+        int currentLatest = 0;
+        try (PreparedStatement pstmt = c.prepareStatement(forumSummaryViewStatement)){
+            rst = pstmt.executeQuery();
+            while(rst.next()){
+                long forumID = rst.getLong("id");
+                String title = rst.getString("title");
+                long latestTopic;
+                String latestTitle;
+                try (PreparedStatement pstmt = c.prepareStatement(topicSummaryStatement)){                    
+                    secondRst = pstmt.executeQuery();
+                    int latestTime = 0;
+                    while(secondRst.next()) {
+                        String title = secondRst.getString("title"); 
+                        long topicID = secondRst.getLong("id");                 
+                        SimpleTopicSummaryView topic = new SimpleTopicSummaryView(topicID, forumID, title));
+                        int currentTime = topic.getLatestPost(c, id).getValue().getPostedAt();
+                        if (currentTime > latestTime) {
+                            latestTopic = topicID;
+                            latestTime = currentTime;
+                            latestTitle = secondRst.getString("title");
+                        }
+                    }
+                list.add(new ForumSummaryView(forumID, title, new SimpleTopicSummaryView(forumID, latestTopic, latestTitle));                    
+                catch (SQLException e) {
+                    return Result.fatal("Unknown error");
+                }
+            }            
+        }
+        catch (SQLException e) {
+            return Result.fatal("Unknown error");
+        }
+
     }
 
 }

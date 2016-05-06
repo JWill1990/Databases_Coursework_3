3dd7b240e5bd62a95462503439efb009a38e128d
xtrememib01 <xtrememib01@gmail.com>
2016-05-06 12:14:24 +0100

    Added create new forum function

--- bb/src/uk/ac/bris/cs/databases/cwk3/Forum.java
+++ bb/src/uk/ac/bris/cs/databases/cwk3/Forum.java
@@ -19,20 +19,28 @@ import uk.ac.bris.cs.databases.api.ForumView;
 
 public class Forum {
 
-    private final static String simpleForumSummaryViewStatement = 
+    private final static String simpleForumSummaryViewStatement =
         "SELECT * FROM Forum ORDER BY Forum.title";
-    private final static String topicSummaryStatement = 
+
+    private final static String topicSummaryStatement =
         "SELECT Topic.id, Topic.title FROM Topic " +
         "JOIN Forum ON Forum.id = Topic.forumID " +
         "WHERE Forum.id=?";
+
     private static final String getDetailedForumSQL =
         "SELECT Forum.title AS forumTitle, Forum.id AS forumID, " +
         "Topic.id AS topicID, Topic.title AS topicTitle " +
         "FROM Topic " +
         "JOIN Forum ON Topic.forumID=Forum.id " +
         "WHERE Forum.id=?";
+
+    private static final String countForum=
+    		"SELECT * from Forum";
+
+    private static final String addForum=
+    		"INSERT into Forum values (?, ?)";
 /*
-    private static final String getAdvancedForumsSQL = 
+    private static final String getAdvancedForumsSQL =
         "SELECT Forum.id AS ForumID, Forum.title AS ForumTitle, Topic.id AS TopicID,  " +
         "Topic.title AS TopicTitle FROM Topic " +
         "JOIN Forum ON Topic.forumID=Forum.id " +
@@ -40,12 +48,12 @@ public class Forum {
     private static final String latestPostSQL =
         "SELECT Forum.id AS Forum_id, Topic.id AS Topic_id, Person.name, " +
         "Person.username, Post.text, Post.postedAt, " +
-        "FROM Post " + 
+        "FROM Post " +
         "JOIN Topic ON Post.topicID=Topic.id " +
         "JOIN Forum ON Topic.forumID=Forum.id " +
         "WHERE Topic.id=?" +
         "ORDER BY Post.postedAt";
-    private static final String getLikersStatement = 
+    private static final String getLikersStatement =
         "SELECT count(*) " +
         "FROM Person " +
         "JOIN TopicLikers ON TopicLikers.personID = Person.username " +
@@ -58,7 +66,7 @@ public class Forum {
      * by title. Simple version that does not return any topic information.
      * @return the list of all forums; an empty list if there are no forums.
      */
-    public static  Result<List<SimpleForumSummaryView>> getSimpleSummary(Connection c){ 
+    public static  Result<List<SimpleForumSummaryView>> getSimpleSummary(Connection c){
         ResultSet rst;
         ArrayList list = new ArrayList();
         try (PreparedStatement pstmt= c.prepareStatement(simpleForumSummaryViewStatement)){
@@ -89,7 +97,7 @@ public class Forum {
      */
     public static Result<List<ForumSummaryView>> getSummary(Connection c)
     {
-        
+
         ResultSet rst;
         ResultSet secondRst;
         ArrayList<ForumSummaryView> forumList = new ArrayList<ForumSummaryView>();
@@ -101,25 +109,25 @@ public class Forum {
                 String forumTitle = rst.getString("title");
                 long latestTopic = 0;
                 String latestTopicTitle = "";
-                try (PreparedStatement secondPstmt = c.prepareStatement(topicSummaryStatement)) { 
-                    secondPstmt.setLong(1, forumID);                   
+                try (PreparedStatement secondPstmt = c.prepareStatement(topicSummaryStatement)) {
+                    secondPstmt.setLong(1, forumID);
                     secondRst = secondPstmt.executeQuery();
                     int latestTime = 0;
                     while(secondRst.next()) {
-                        String topicTitle = secondRst.getString("title"); 
-                        long topicID = secondRst.getLong("id");                 
+                        String topicTitle = secondRst.getString("title");
+                        long topicID = secondRst.getLong("id");
                         SimpleTopicSummaryView topic = new SimpleTopicSummaryView(topicID, forumID, topicTitle);
                         int currentTime = 0;
-                        /*int currentTime = Topic.getLatestPost(c, topicID).getValue().getPostedAt();   
-                        System.out.println("here?");  */                  
+                        /*int currentTime = Topic.getLatestPost(c, topicID).getValue().getPostedAt();
+                        System.out.println("here?");  */
                         if (currentTime >= latestTime) {
                             latestTopic = topicID;
                             latestTime = currentTime;
                             latestTopicTitle = topicTitle;
                         }
                     }
-                    forumList.add(new ForumSummaryView(forumID, forumTitle, new SimpleTopicSummaryView(forumID, latestTopic, latestTopicTitle))); 
-                }                   
+                    forumList.add(new ForumSummaryView(forumID, forumTitle, new SimpleTopicSummaryView(forumID, latestTopic, latestTopicTitle)));
+                }
                 catch (SQLException e) {
                     return Result.fatal("Unknown error");
                 }
@@ -129,7 +137,7 @@ public class Forum {
             }
             else {
                 return Result.failure("There are no forums for this table");
-            }				          
+            }
         }
         catch (SQLException e) {
             return Result.fatal("Unknown error");
@@ -144,26 +152,64 @@ public class Forum {
      */
     public static Result<ForumView> getDetailedForum(Connection c, long forumId)
     {
-        ResultSet rst; 
-        ArrayList<SimpleTopicSummaryView> forumTopics = new ArrayList<SimpleTopicSummaryView>();  
+        ResultSet rst;
+        ArrayList<SimpleTopicSummaryView> forumTopics = new ArrayList<SimpleTopicSummaryView>();
         try (PreparedStatement pstmt = c.prepareStatement(getDetailedForumSQL)){
-            
+
             pstmt.setLong(1, forumId);
-            
+
             rst = pstmt.executeQuery();
-            
-            String forumTitle = rst.getString("ForumTitle"); 
-            while (rst.next()) {                                 
+
+            String forumTitle = rst.getString("ForumTitle");
+            while (rst.next()) {
                 long topicId = rst.getLong("TopicID");
                 String topicTitle = rst.getString("TopicTitle");
-                forumTopics.add(new SimpleTopicSummaryView(topicId, forumId, topicTitle));                   
+                forumTopics.add(new SimpleTopicSummaryView(topicId, forumId, topicTitle));
             }
             ForumView fv = new ForumView(forumId, forumTitle, forumTopics);
-            return Result.success(fv);               		          
+            return Result.success(fv);
         }
-        catch (SQLException e) {            
+        catch (SQLException e) {
             return Result.fatal("Unknown error");
         }
-    } 
+    }
+
+    public static Result addForum(Connection c, String title){
+    	int row=0;
+    	System.out.println(CheckExists.Forum(c, title));
+    	if(!CheckExists.Forum(c, title)){
+
+	           try (PreparedStatement pstmt = c.prepareStatement(countForum)) {
+	               ResultSet rs=pstmt.executeQuery();
+	               while(rs.next()){row++;}/*this counts the total number of exsitingrows*/
+	               System.out.println("TOtal no of rows are"+row);
+	           }catch (SQLException e) {
+	               System.out.println("Exception in 1st is "+e);
+	        	   return Result.fatal("Unknown error");
+	               }
+
+	           try(PreparedStatement pstmt=c.prepareStatement(addForum)){
+	        	   pstmt.setInt(1, row+1);
+	        	   pstmt.setString(2, title);
+
+	        	   if(TestValidInput.Validator(title)){
+		        	   pstmt.executeUpdate(); //Use update method to write to db
+		        	   c.commit();
+		        	   return Result.success();
+	        	   	}
+	        	   else {
+	        		   return Result.failure("Invalid String Input");
+	        	   }
+
+	           }catch(Exception e){
+	        	   System.out.println("Exception in 2nd is "+e);
+	        	   return Result.failure("Unexpected failure");
+	        	   }
+	           }
+    	else{
+    		return Result.failure("Forum exists");
+    	}
+
+    }
 
 }
